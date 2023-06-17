4aae8c78177751c957f820c6daf0f762d9880d29
MikeAde07 <MikeAde07@gmail.com>
2023-06-17 17:16:45 -0500

    test

---
+++ Develop/date.js
@@ -0,0 +1,5 @@
+//$(".lead").text = moment().format("[Today is] dddd, MMMM Do YYYY");
+//var date = moment().format("[Today is] dddd, MMMM Do YYYY");
+//$("#currentDay").append(date);
+console.log("Michael");
+console.log(3);
